3fdfbf45852783f7751415a7c83ebedb5374b771
Rob Canfield <git@livesenjoyable.com>
2019-08-21 16:01:52 -0600

    cleaned up bootstrap layout with padding and margins, added firebase in JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDzt8c4lw8wcLDSVFl-6rtKiTash7ifmrc",
+    authDomain: "train-schedule-2e3dc.firebaseapp.com",
+    databaseURL: "https://train-schedule-2e3dc.firebaseio.com",
+    projectId: "train-schedule-2e3dc",
+    storageBucket: "",
+    messagingSenderId: "22234897843",
+    appId: "1:22234897843:web:7ca2e43e27d6d837"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
