af07b9d76b3fe8b8f6ea3d139dbee7c465ce6029
Rob Canfield <git@livesenjoyable.com>
2019-11-30 19:32:21 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # 07-Train-Schedule
-Bootcamp assignment 07 - create an application that allows users to add train schedules that will update in real time.
+
+This application uses Firebase to update the train schedule in real time as trains are added. 
+
+MomentJS was used for time conversions and calculations. 
+
+This project has no backend besides it's connection to Firebase. 
+
+All HTML is written in index.html, pulling CSS, JS, and Images from the Assets folder. 
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,7 +13,7 @@
 
   var database = firebase.database();
 
-  $("#submit").on('click', function(){
+  $("#submit").on('click', function(event){
     event.preventDefault();
 
     var trainName = $("#train-name").val().trim();
@@ -57,10 +57,6 @@
 
       var nextTrain = moment().add(nextArrival, "minutes");
 
-
-
-
-
       var newRow = $("<tr>").append(
        $("<td>").text(snapshot.val().name),
        $("<td>").text(snapshot.val().destination),
@@ -69,47 +65,8 @@
        $("<td>").text(nextArrival + " min. away")
       );
   
-  
       $("#train-info").append(newRow);
-  
-
-
    
   }, function(errorObject) {
     console.log("Errors handled: " + errorObject.code);
   });
-
-
-
-    // // Assumptions
-    // var tFrequency = 3;
-
-    // // Time is 3:30 AM
-    // var firstTime = "03:30";
-
-    // // First Time (pushed back 1 year to make sure it comes before current time)
-    // var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-    // console.log(firstTimeConverted);
-
-    // // Current Time
-    // var currentTime = moment();
-    // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
-    // // Difference between the times
-    // var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    // console.log("DIFFERENCE IN TIME: " + diffTime);
-
-    // // Time apart (remainder)
-    // var tRemainder = diffTime % tFrequency;
-    // console.log(tRemainder);
-
-    // // Minute Until Train
-    // var tMinutesTillTrain = tFrequency - tRemainder;
-    // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-    // // Next Train
-    // var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-
-
-
